fcdcf68c6f429abf9afc99ba75c5c1649556865b
Nick Brinegar <44592832+RodleyB@users.noreply.github.com>
2018-11-10 12:40:36 -0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,62 @@
+
+public class Player extends Character
+{
+	
+	public Weapon activeWeapon;		// weapon currently being used
+	public Armor activeArmor;			// armor currently being worn
+	public Player()
+	{
+		
+	}
+	
+	public Player(String name, int health, int mana, int gold, Item[] inventory)
+	{
+		super(name, health, mana, gold, inventory);
+	}
+	
+	public void setActiveWeapon(int inventoryIndex)
+	{ // checks to ensure index is in list and is a weapon
+		if (inventoryIndex > this.inventory.length - 1)
+		{
+			System.out.println("Index given is not in inventory");
+		} else if (inventoryIndex < 0)
+		{
+			System.out.println("Index given is not in inventory");
+		}
+		
+		if (this.inventory[inventoryIndex] instanceof Weapon)
+		{
+			this.activeWeapon = (Weapon) this.inventory[inventoryIndex];
+		}
+	}
+	
+	public void setActiveArmor(int inventoryIndex)
+	{ 
+		if (inventoryIndex > this.inventory.length - 1)
+		{
+			System.out.println("Index given is not in inventory");
+		} else if (inventoryIndex < 0)
+		{
+			System.out.println("Index given is not in inventory");
+		}
+		
+		if (this.inventory[inventoryIndex] instanceof Armor)
+		{
+			this.activeArmor = (Armor) this.inventory[inventoryIndex];
+		}
+	}
+	
+	public boolean addItem(Item item)
+	{
+		for (int i = 0; i < this.inventory.length; i++)
+		{
+			if (this.inventory[i] == null)
+			{
+				this.inventory[i] = item;
+				return true;		// successfully added the item
+			}
+		}
+		// if it gets to this point then inventory is full
+		return false;
+	}
+}
