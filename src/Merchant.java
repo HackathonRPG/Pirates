e710738cd1769bedcaf6e0c86f268938415f46a2
Nick Brinegar <44592832+RodleyB@users.noreply.github.com>
2018-11-10 15:23:59 -0700

    Add files via upload

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Merchant extends Character {
 	public Item[] wares;
@@ -12,16 +13,42 @@ public class Merchant extends Character {
 		super(name, health, 0, 0, wares);		// mana and gold irrelevant for merchants
 	}
 	
-	public Item sellItem(int itemIndex)
+	public void merchantInteraction(Player player)
 	{
-		if (itemIndex < 0 || itemIndex > this.inventory.length)
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to my shop!\nEnter 1 to see wares\nEnter 2 to sell\nor 0 to leave");
+		int menuChoice = input.nextInt();
+		if (menuChoice == 1)
 		{
-			return null;
-		} else 
-		{
-			Item boughtItem = this.inventory[itemIndex];
-			this.inventory[itemIndex] = null;
-			return boughtItem;
+			this.printInventory();
+			System.out.println("Enter the number of item you would like to buy");
+			int desiredItem = input.nextInt();
+			if (desiredItem < 0 || desiredItem > this.inventory.length)
+			{
+				
+			} else 
+			{
+				Item boughtItem = this.inventory[desiredItem];
+				this.inventory[desiredItem] = null;
+				player.addItem(boughtItem);
+			}
 		}
+		else if (menuChoice == 2)
+		{
+			player.printInventory();
+			System.out.println("Enter the number of the item you would like to sell");
+			int desiredItem = input.nextInt();
+			if (desiredItem < 0 || desiredItem > this.inventory.length)
+			{
+				
+			} else 
+			{
+				Item soldItem = player.inventory[desiredItem];
+				player.inventory[desiredItem] = null;
+				this.addItem(soldItem);
+			}
+		}
+		
 	}
+
 }
